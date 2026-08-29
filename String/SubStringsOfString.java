e317a7272ba1a4b335a065c2f6a7aa7dc0c94fbb
Siddharth Gautam <siddhrthgutam@gmail.com>
2026-08-30 02:51:58 +0530

    Create SubStringsOfString.java

---
+++ String/SubStringsOfString.java
@@ -0,0 +1,11 @@
+public class SubStringsOfString {
+    public static void main(String[] args) {
+        String s = "bhakti";
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                System.out.print(s.substring(i,j+1)+" "); // i to j
+            }
+            System.out.println();
+        }
+    }
+}
