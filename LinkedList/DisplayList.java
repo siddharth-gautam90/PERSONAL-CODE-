dec1e7eab3e3b209b7635d157d3a4902f465ecbf
Siddharth Gautam <siddhrthgutam@gmail.com>
2026-09-01 20:58:35 +0530

    Update DisplayList.java

--- LinkedList/DisplayList.java
+++ LinkedList/DisplayList.java
@@ -1,1 +1,56 @@
 
+class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+    }
+    
+}
+public class DisplayList {
+    public static void displayRec(Node head){
+        if(head == null) return;
+        displayRec(head.next);
+        System.out.print(head.val+" ");
+    }
+    public static void display(Node head){
+        // Node temp = head;
+        // while(temp != null){ // Elements create using while loop
+        //     System.out.print(temp.val+" ");
+        //     temp = temp.next; // looks like temp++
+        // }
+       for(Node temp = head;temp != null;temp = temp.next){ //Elements create using while loop
+           System.out.print(temp.val+" ");
+       }
+       System.out.println();
+    }
+    public static void main(String[] args) {
+        Node a = new Node(37); // head node
+        Node b = new Node(478);
+        Node c = new Node(273);
+        Node d = new Node(79);
+        Node e = new Node(387);
+        a.next = b; b.next = c;
+        c.next = d; d.next = e;
+        // display(a);
+        // displayRec(a);
+        System.out.println(get(a, 4));
+        
+       Node n = null;
+       n.val = 10;
+       System.out.println(n.val);
+       System.out.println(n.next);
+    }
+
+    private static int get(Node head, int idx) {
+        for(Node temp = head;temp != null;temp = temp.next){ //Elements create using while loop
+           System.out.print(temp.val+" ");
+       }
+        Node temp = head;
+        for(int i=1;i<=idx;i++){
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+}
